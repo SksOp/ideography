5fd9a9feb8f745825483f9420f86417adba79582
shubhamCodeEngine <shubham.k@code-engine.net>
2024-04-28 22:53:03 +0530

    feat: modified openai package

---
+++ packages/openai/gpt.go
@@ -0,0 +1,68 @@
+package openai
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Config struct {
+	OrgId     string
+	ProjectId string
+	ApiKey    string
+}
+
+func CreateGPtInstance(c Config) *OpenAi {
+	return &OpenAi{c, "gpt-3.5-turbo", "https://api.openai.com/v1/chat/completions", 0.7}
+}
+
+type OpenAi struct {
+	config      Config
+	model       string
+	url         string
+	temperature float64
+}
+
+func (g *OpenAi) SetModel(model string) *OpenAi {
+	g.model = model
+	return g
+}
+
+func (g *OpenAi) SetTemperature(temperature float64) *OpenAi {
+	g.temperature = temperature
+	return g
+}
+
+func (g *OpenAi) Query(messages []Message) (*GptResponse, error) {
+
+	body := g.CreateQueryBody(messages)
+
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("POST", g.url, bytes.NewReader(body))
+	authheader := fmt.Sprintf("Bearer %v", g.config.ApiKey)
+
+	req.Header.Set("Authorization", authheader)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	b, _ := io.ReadAll(resp.Body)
+
+	var response GptResponse
+
+	if err := json.Unmarshal(b, &response); err != nil {
+		fmt.Println(err)
+
+		return nil, err
+	}
+	return &response, nil
+
+}
---
+++ packages/openai/message.go
@@ -0,0 +1,37 @@
+package openai
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type QueryBody struct {
+	Model       string    `json:"model"`
+	Messages    []Message `json:"messages"`
+	Temperature float64   `json:"temperature"`
+}
+
+func CreateUserMessageObject(q string) Message {
+	return Message{Role: "user", Content: q}
+}
+
+func CreateSystemMessageObject(q string) Message {
+	return Message{Role: "system", Content: q}
+
+}
+
+func (g *OpenAi) CreateQueryBody(q []Message) []byte {
+	qb := &QueryBody{
+		Model:       g.model,
+		Temperature: g.temperature,
+		Messages:    q,
+	}
+	fmt.Println(qb)
+	body, _ := json.Marshal(qb)
+	return body
+}
